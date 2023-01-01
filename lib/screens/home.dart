9326d605f05b9b54fb4616a49d1cba2b78754b6d
ocotorse <allouchhatim@gmail.com>
2023-01-01 17:40:18 +0100

    Onboarding screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,10 @@ import 'package:flutter_application_1/screens/menu.dart';
 import 'package:flutter_application_1/screens/LoginPage.dart';
 import 'package:flutter/material.dart';
 import 'LoginPage.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/Widgets/bottombar.dart';
+import 'package:flutter_application_1/screens/menu.dart';
+import 'package:flutter_application_1/screens/Onboarding.dart';
 
 class home extends StatefulWidget {
   const home({super.key});
